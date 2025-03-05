761741b11974021fa2dd22537e0d16a226a0ee7a
Burak Gençer <bggithub@proton.me>
2025-03-05 22:19:05 +0300

    Added ListScreen.kt and used LazyColumn for FoodList

---
+++ app/src/main/java/com/burak/cookbookcompose/ListScreen.kt
@@ -0,0 +1,31 @@
+package com.burak.cookbookcompose
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.burak.cookbookcompose.ui.theme.CookBookComposeTheme
+
+@Composable
+fun FoodList() {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+
+        items(count = 15) { index ->
+            Row{
+                Text(text = "Item: $index")
+            }
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FoodListPreview() {
+    CookBookComposeTheme {
+        FoodList()
+    }
+}
